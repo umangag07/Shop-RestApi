4208c78c42dbb7363ab19af9572ca75c5b047b4c
umang <25umangagarwal@gmail.com>
2020-11-29 16:53:32 +0530

    connected the database

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+require('dotenv/config')
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
@@ -11,11 +13,15 @@ app.use((req,res,next)=>{
         res.header('Access-Control-Allow-Methods','PUT, POST, GET, PATCH, DELETE')
         return res.status(200).json({})
     }
+    next();
 })
 const ProductRoute = require('./Routes/Product');
 app.use('/products',ProductRoute)
 const OrderRoute = require('./Routes/Orders');
 app.use('/orders',OrderRoute)
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true  }, ()=>{
+    console.log('connected to database')
+})
 
 app.use((req,res,next)=>{
     res.status(200).json({
