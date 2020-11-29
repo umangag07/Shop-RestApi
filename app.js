453b6d098d05ac3f08e21a2dad2372aeaaac9e23
umang <25umangagarwal@gmail.com>
2020-11-29 16:23:07 +0530

    Added the cors header

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT, POST, GET, PATCH, DELETE')
+        return res.status(200).json({})
+    }
+})
 const ProductRoute = require('./Routes/Product');
 app.use('/products',ProductRoute)
 const OrderRoute = require('./Routes/Orders');
