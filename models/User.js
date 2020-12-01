53442b47cacbc4906edf0e3101fcad681b91cf3c
umang <25umangagarwal@gmail.com>
2020-12-01 17:16:30 +0530

    Added the validation for the email

--- models/User.js
+++ models/User.js
@@ -3,7 +3,9 @@ const UserSchema = mongoose.Schema({
     _id:mongoose.Schema.Types.ObjectId,
     email:{
         type: String,
-        required:true
+        required:true,
+        unique:true,
+        match:/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
     },
     password:{
           type:String,
