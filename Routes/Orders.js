a207a4c4a6f8d967e670bbf7d16d40cd3d03f385
umang <25umangagarwal@gmail.com>
2020-12-02 17:32:36 +0530

    Made controller for the order which handle all the fucntionality of the request

--- Routes/Orders.js
+++ Routes/Orders.js
@@ -4,91 +4,16 @@ const Order = require('../models/Order')
 const Product = require('../models/Product')
 const mongoose = require('mongoose')
 const checkAuth = require('../middleware/check-auth')
+const OrderController = require('../controllers/order')
 
-router.get('/',checkAuth,async (req ,res)=>{
-    try{
-        const orders = await Order.find()
-        res.send(orders)
-    }catch(err){
-        res.send({message:err})
-    }
-})
+router.get('/',checkAuth, OrderController.get_all_order)
 
-router.post('/',checkAuth,(req, res)=>{
-    Product.findById(req.body.productId)
-    .then(product=>{
-        const order= new Order({
-            _id: mongoose.Types.ObjectId(),
-            product: req.body.productId,
-            quantity:req.body.quantity,
-            productUrl:"http://localhost:3000/products/"+req.body.productId
-      
-          })
-          order.save()
-          .then(result=>{
-              console.log(result)
-              res.send(result)
-          })
-          .catch(err=>{
-              res.send({message:err})
-          })
-    })
-    .catch(err=>{
-        res.status(500).json({
-            message:'product not found',
-            error:err
-        })
-    })
-    
-})
+router.post('/',checkAuth, OrderController.post_order)
 
-router.get('/:orderId',checkAuth,(req, res)=>{
-    const id = req.params.orderId;
-         Order.findById(id)
-         .exec()
-         .then(result=>{
-             if(!result){
-                 res.status(404).json({message:"product not found"})
-             }
-             else{
-                res.send(result)
-             }
-             
-         })
-         .catch(err=>{
-             res.send(err)
-         })
-    
-})
+router.get('/:orderId',checkAuth, OrderController.get_single_order)
 
-router.patch('/:orderId',checkAuth,(req, res)=>{
-    const id = req.params.orderId;
-    const orderPatch = {}
-    console.log(req.body)
-    for(const ord of req.body){
-        orderPatch[ord.propName] = ord.value
-    }
-    Order.updateOne({_id:id},{$set:orderPatch})
-    .then(result=>{
-        res.send(result)
-    })
-    .catch(err=>{
-        res.send(err)
-    })
-        
-    
-})
+router.patch('/:orderId',checkAuth, OrderController.patch_order)
 
-router.delete('/:orderId',checkAuth,(req, res)=>{
-    const id = req.params.orderId;
-    Order.remove({_id:id})
-    .then(result=>{
-        res.send(result)
-    })
-    .catch(err=>{
-        res.send(err)
-    })
-    
-})
+router.delete('/:orderId',checkAuth, OrderController.delete_order)
 
 module.exports = router
---
+++ controllers/order.js
@@ -0,0 +1,85 @@
+const Order = require('../models/Order')
+const Product = require('../models/Product')
+const mongoose = require('mongoose')
+
+exports.get_all_order = async (req ,res)=>{
+    try{
+        const orders = await Order.find()
+        res.send(orders)
+    }catch(err){
+        res.send({message:err})
+    }
+}
+exports.post_order = (req, res)=>{
+    Product.findById(req.body.productId)
+    .then(product=>{
+        const order= new Order({
+            _id: mongoose.Types.ObjectId(),
+            product: req.body.productId,
+            quantity:req.body.quantity,
+            productUrl:"http://localhost:3000/products/"+req.body.productId
+      
+          })
+          order.save()
+          .then(result=>{
+              console.log(result)
+              res.send(result)
+          })
+          .catch(err=>{
+              res.send({message:err})
+          })
+    })
+    .catch(err=>{
+        res.status(500).json({
+            message:'product not found',
+            error:err
+        })
+    })
+    
+}
+exports.get_single_order = (req, res)=>{
+    const id = req.params.orderId;
+         Order.findById(id)
+         .exec()
+         .then(result=>{
+             if(!result){
+                 res.status(404).json({message:"product not found"})
+             }
+             else{
+                res.send(result)
+             }
+             
+         })
+         .catch(err=>{
+             res.send(err)
+         })
+    
+}
+exports.patch_order = (req, res)=>{
+    const id = req.params.orderId;
+    const orderPatch = {}
+    console.log(req.body)
+    for(const ord of req.body){
+        orderPatch[ord.propName] = ord.value
+    }
+    Order.updateOne({_id:id},{$set:orderPatch})
+    .then(result=>{
+        res.send(result)
+    })
+    .catch(err=>{
+        res.send(err)
+    })
+        
+    
+}
+exports.delete_order = (req, res)=>{
+    const id = req.params.orderId;
+    Order.remove({_id:id})
+    .then(result=>{
+        res.send(result)
+    })
+    .catch(err=>{
+        res.send(err)
+    })
+    
+}
